9d5be4fae34e1f0c353669f64e1a891b67822624
Liam VanScoyoc <liamvanscoyoc@gmail.com>
2023-04-06 07:00:35 -0400

    Updated for APTIV field at FoM Championship

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023mifor",
+      "defaultValue": "2023micmp3",
       "required": "true",
       "disabled": "true"
     },
