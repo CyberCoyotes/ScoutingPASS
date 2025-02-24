6e50b65ae7dddd55e32253cee14cf3a773571444
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-02-23 21:08:27 -0500

    Update authentication key in TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "EZox9Kh53Xq0ouw59KuLolUPMV4l2b8YyFRLtxE9L8Isj2qGJLML3X7VstHWGI2d";
 /**
  * Get list of teams in event
  *
